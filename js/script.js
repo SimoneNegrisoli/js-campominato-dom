a1a142841c500800368e4bd3f216275b31385e94
SimoneNegrisoli <simo.negrisoli@gmail.com>
2023-10-19 19:09:31 +0200

    msg

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 // Consegna
 
-// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
 // In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
 // La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+
+
 // BONUS :100::
-// Aggiungere una `select` accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:  difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe; diifficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe; - difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
 // Quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle. Evitare che si possa cliccare 2 volte sulla stessa cella
 // Quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste.
-// Consigli del giorno:** :party_wizard:
-// Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-// Ad esempio:
-// Di cosa ho bisogno per generare i numeri?
-// Proviamo sempre prima con dei `console.log()` per capire se stiamo ricevendo i dati giusti.
+
 // Le validazioni e i controlli possiamo farli anche in un secondo momento.
 
 
@@ -29,6 +27,9 @@ function campoMinatoGame() {
     function campoMinato() {
         const level = document.getElementById('level').value;
         const numSquare = selectLevel(level);
+
+        let score = 0;
+        
 
         const playground = document.querySelector('.playground');
         playground.innerHTML = '';
@@ -54,15 +55,17 @@ function campoMinatoGame() {
 
 
         for (let i = 0; i < numSquare; i++) {
-            const square = createSquare(i, numSquare,bombs);
+            const square = createSquare(i, numSquare, bombs,score);
             playground.appendChild(square);
         }
 
         console.log(bombs)
-        
+
     }
 
-    function createSquare(squareInd, numSquare, bombs) {
+    function createSquare(squareInd, numSquare, bombs,score) {
+        let play = true;
+
         const squareWidth = Math.sqrt(numSquare);
         const square = document.createElement('div');
         square.classList.add('square');
@@ -70,37 +73,33 @@ function campoMinatoGame() {
         square.style.width = `calc(100% / ${squareWidth})`;
         square.style.height = square.style.width;
 
-       
-        square.addEventListener('click', clickSquare)
-        
-        function clickSquare () {
+
+        square.addEventListener('click', function () {
+            let msg;
+            if(!play) {
+                return;
+            }
+
             if (bombs.includes(squareInd + 1)) {
                 square.classList.add('death')
                 square.innerHTML = '<i class="fa-solid fa-bomb fa-beat" style="color: #000000;"></i>';
-                gameOver();
+                play = false;
+                msg = `Hai perso: Il tuo punteggio è ${score}`
             } else {
                 square.classList.add('squarenew')
                 square.style.color = 'black';
+                score++
+                if (score === (numSquare - 16)){
+                    msg = `Hai vinto facendo il punteggio massimo`
+                    play = false;
+                }else{
+                    msg = `Il tuo punteggio attuale è: ${score}`
+                }
             }
-        }
-            
+            document.getElementById('score').innerHTML = msg;
 
-        
-
+        })
         return square;
-
-        function gameOver() {
-            const squares = document.querySelectorAll('.square');
-            for (let i = 0; i < squares.length; i++) {
-                squares[i].removeEventListener('click', clickSquare);
-            }
-        
-            alert('Game Over! Hai calpestato una bomba.');
-        }
-
-
-
-
     }
 
     function selectLevel(level) {
